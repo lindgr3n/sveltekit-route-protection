926154ee8555dfd3c2bb4d3cdfab83e2d8e6501b
Johan Lindgren <johan@lindgr3n.com>
2022-11-28 19:43:35 +0100

    feat: headers

---
+++ src/lib/components/templates/AuthenticatedHeader.svelte
@@ -0,0 +1,25 @@
+<script>
+	import { invalidateAll } from '$app/navigation';
+	import { signOut } from '@lucia-auth/sveltekit/client';
+
+	async function onSignOut() {
+		await signOut();
+		invalidateAll();
+	}
+</script>
+
+<div class="flex items-center justify-between px-4 py-6 sm:px-6 md:justify-start md:space-x-10">
+	<div class="flex justify-start lg:w-0 lg:flex-1"><span class="hidden">Your logo here</span></div>
+
+	<div class="hidden items-center justify-end md:flex md:flex-1 lg:w-0">
+		<a
+			href="/dashboard"
+			class="whitespace-nowrap text-base font-medium text-gray-500 hover:text-gray-900">Dashboard</a
+		>
+		<a
+			href="/profile"
+			class="whitespace-nowrap text-base font-medium text-gray-500 hover:text-gray-900">Profile</a
+		>
+		<button on:click={onSignOut}>Logout</button>
+	</div>
+</div>
---
+++ src/lib/components/templates/Header.svelte
@@ -0,0 +1,15 @@
+<div class="flex items-center justify-between px-4 py-6 sm:px-6 md:justify-start md:space-x-10">
+	<div class="flex justify-start lg:w-0 lg:flex-1"><span class="hidden">Your logo here</span></div>
+
+	<div class="hidden items-center justify-end md:flex md:flex-1 lg:w-0">
+		<a
+			href="/login"
+			class="whitespace-nowrap text-base font-medium text-gray-500 hover:text-gray-900">Sign in</a
+		>
+		<a
+			href="/register"
+			class="ml-8 inline-flex items-center justify-center whitespace-nowrap rounded-md border border-transparent bg-blue-600 px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-blue-700"
+			>Register</a
+		>
+	</div>
+</div>
